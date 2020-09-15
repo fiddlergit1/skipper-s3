6d0989413cd35299ba6a75347ad4872780ca5599
Jussi Kaijalainen <jussi@wingmen.fi>
2020-09-15 12:01:56 +0800

    Add support for passing httpOptions to aws-sdk

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ var AWS = require('aws-sdk');
  *         @property {String} endpoint
  *         @property {String} sslEnabled
  *         @property {String} s3ForcePathStyle
+ *         @property {Object} httpOptions
  *
  * @returns {Dictionary}
  *         @property {Function} read
@@ -212,7 +213,8 @@ function _buildS3Client(s3ClientOpts) {
     secretAccessKey: s3ClientOpts.secret,
     endpoint: s3ClientOpts.endpoint,
     sslEnabled: s3ClientOpts.sslEnabled,
-    s3ForcePathStyle: s3ClientOpts.s3ForcePathStyle
+    s3ForcePathStyle: s3ClientOpts.s3ForcePathStyle,
+    httpOptions: s3ClientOpts.httpOptions
   });
   return new AWS.S3(s3ConstructorArgins);
 }//ƒ
